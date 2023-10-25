cc3e731703e8ebc587c634dd70eba53f220ed95e
wontae.choi <35890502+wontae913@users.noreply.github.com>
2023-10-25 11:39:00 +0900

    Create change.sh
    japgi

---
+++ change.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+FILENAME="$1"
+
+while read line
+do
+    SERVER=$(echo -e $line | awk '{print $1}')
+    echo -e "Server Name = $SERVER"
+
+    PRIVATE_IP=$(echo -e $line | awk '{print $2}')
+    echo -e "Private IP = $PRIVATE_IP"
+
+    USER=$(echo -e $line | awk '{print $3}')
+    echo -e "User Name = $USER"
+
+    KEY=$(echo -e $line | awk '{print $4}')
+    echo -e "KEY File Name = $KEY"
+
+    PORT=$(echo -e $line | awk '{print $5}')
+    echo -e "SSH Port = $PORT"
+
+    NEWKEY=$(echo -e $line | awk '{print $6}')
+    echo -e "New-Key File Name = $NEWKEY"
+
+    KEYVALUE=$(ssh-keygen -y -f $NEWKEY)
+    echo -e "Changing '$SERVER' key value"
+    echo $KEYVALUE
+
+    if [ "" == "$KEYVALUE" ]; then
+        echo "invaild keyvalue"
+        exit
+    else
+        sudo echo $KEYVALUE | ssh -o StrictHostKeyChecking=no -i $KEY $USER@$PRIVATE_IP -p $PORT "sudo runuser -l $USER -c 'cat > /home/$USER/.ssh/authorized_keys'"
+    fi
+
+    echo -e ""
+    sleep 3
+
+done < $FILENAME
